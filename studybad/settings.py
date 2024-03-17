592e300a36346b18189c7f9c3101cf4985f97f9b
vverac <viverace@gmail.com>
2024-03-16 21:58:29 -0300

    registro de app base; prueba de rutas

--- studybad/settings.py
+++ studybad/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'base',
+    'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
--- studybad/urls.py
+++ studybad/urls.py
@@ -16,7 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http  import HttpResponse
+
+
+def home(request):
+    return HttpResponse ("Home Page") 
+
+
+def room(request):
+    return HttpResponse ("ROOM") 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", home, name="home"),
+    path("room/", room, name="room"),
 ]
