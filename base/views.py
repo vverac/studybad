351c2e12602527f4d26d57b96ba860461f0945b1
vverac <viverace@gmail.com>
2024-03-18 22:50:30 -0300

    trabajando con herencia de plantillas, extendiendo main.html a home.html y room.html

--- base/views.py
+++ base/views.py
@@ -3,10 +3,17 @@ from django.http  import HttpResponse
 
 
 # Create your views here.
+rooms=[
+    {'id':1, 'name':'lets learn python!'},
+    {'id':1, 'name':'design with me'},
+    {'id':1, 'name':'frontend developer'},
+]
+
 
 def home(request):
     #return HttpResponse ("Home Page") 
-    return  render (request, 'home.html')
+    # return  render (request, 'home.html')
+     return  render (request, 'home.html', {'rooms': rooms})
 
 
 def room(request):
