dd8dc27b02e599aac9f678984059a6197ebde65b
vverac <viverace@gmail.com>
2024-03-18 23:13:29 -0300

    usando diccionario como context, renderizando el diccionario en la platilla home.html con el bucle for

--- base/views.py
+++ base/views.py
@@ -5,16 +5,17 @@ from django.http  import HttpResponse
 # Create your views here.
 rooms=[
     {'id':1, 'name':'lets learn python!'},
-    {'id':1, 'name':'design with me'},
-    {'id':1, 'name':'frontend developer'},
+    {'id':2, 'name':'design with me'},
+    {'id':3, 'name':'frontend developer'},
 ]
 
 
 def home(request):
     #return HttpResponse ("Home Page") 
     # return  render (request, 'home.html')
-     return  render (request, 'home.html', {'rooms': rooms})
-
+    # return  render (request, 'home.html', {'rooms': rooms})
+    context = {'rooms': rooms}
+    return  render (request, 'home.html', context)
 
 def room(request):
     #return HttpResponse ("ROOM") 
