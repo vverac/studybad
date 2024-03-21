0ee72f1f42786cf3a2643337eb0ee2aaadf5682b
vverac <viverace@gmail.com>
2024-03-20 23:03:04 -0300

    cree delete.html y lo use en home.html,

--- base/urls.py
+++ base/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     
     path("room_page/<str:pk>/", views.room, name="room"),
 
-    path('create-room', views.createRoom, name='create-room' )
+    path('create-room', views.createRoom, name='create-room' ),
+    path('update-room/<str:pk>/', views.updateRoom, name='update-room' ),
+    path('delete-room/<str:pk>/', views.deleteRoom, name='delete-room' ),
 ]
