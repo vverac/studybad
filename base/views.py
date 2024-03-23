6bf99f1e50104e6b70d84358fa3be50e30d12ef7
vverac <viverace@gmail.com>
2024-03-23 20:17:06 -0300

    creando la funcionalidad de busqueda a traves de una barra lateral y un input desde navbar

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http  import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
+from django.db.models  import Q
 
 
 # Create your views here.
@@ -16,12 +17,19 @@ def home(request):
     #return HttpResponse ("Home Page") 
     # return  render (request, 'home.html')
     # return  render (request, 'home.html', {'rooms': rooms})
-    rooms=Room.objects.all()    
+    # rooms=Room.objects.all()
+
+    q= request.GET.get('q') if  request.GET.get('q') != None else ''
+    rooms=Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        ) 
 
     topics=Topic.objects.all()
+    room_count=rooms.count() 
 
-
-    context = {'rooms': rooms, 'topics' : topics}
+    context = {'rooms': rooms, 'topics' : topics, 'room_count' : room_count}
     # return  render (request, 'home.html', context)
     return  render (request, 'base/home.html', context)
 
