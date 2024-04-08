ae1ea5c6779872595899446483b3e42f562ef896
vverac <viverace@gmail.com>
2024-04-08 13:16:30 -0400

    cambio en el models Room, modifique la views room, cree una vista deleteMessage, cree el path para deleteMessage, trabaje en room.html

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http  import HttpResponse
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from  django.contrib.auth.decorators import login_required
 from django.db.models  import Q
@@ -90,7 +90,19 @@ def room(request,pk):
     #     if i['id'] == int(pk):
     #         room=i
     room=Room.objects.get(id=pk)  
-    context = {'room': room}        
+    room_messages=room.message_set.all().order_by('-created')
+    participants=room.participants.all()
+
+    if request.method=='POST':
+        message=Message.objects.create(
+            user=request.user,
+            room=room,
+            body=request.POST.get('body'),
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {'room': room, 'room_messages': room_messages, 'participants':participants}        
     #return  render (request,'base/room.html')
     return  render (request,'base/room.html', context)
 
@@ -139,3 +151,17 @@ def deleteRoom(request,pk):
         return redirect('home')
 
     return render(request,  'base/delete.html', {'obj':room})
+
+
+@login_required(login_url='login')   
+def deleteMessage(request,pk):
+    message=Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return  HttpResponse("Your are not allower here!")
+ 
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+
+    return render(request,  'base/delete.html', {'obj':message})
